78a4f069e02beefa0a3c9e127caa8cf654505314
unknown <ritsuki-i>
2024-07-05 05:09:44 +0900

    'deploy'

--- src/index.jsx
+++ src/index.jsx
@@ -13,7 +13,7 @@ import NutritionResult from "./NutritionResult.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router basename="">
+    <Router basename="/cook_with">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ingredients" element={<Ingredients />} />
